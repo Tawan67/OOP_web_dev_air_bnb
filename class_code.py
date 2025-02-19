7542fac51b0327923bf5547e495b9874f02cd1a2
Tawan67 <tawankhuntha@gmail.com>
2025-02-19 23:54:58 +0700

    add all class

--- class_code.py
+++ class_code.py
@@ -1,10 +1,59 @@
 class ControlSystem:
+
     pass
 
 
 class User:
+    count_id = 0
+
+    def __init__(self, id, name, phone_num, age, payment_med):
+        self.__user_id = id
+        self.__user_name = name
+        self.__user_phone_num = phone_num
+        self.__user_age = age
+        self.__user_payment_methhod = payment_med
     pass
 
 
 class Member(User):
+    pass
+
+
+class Host(User):
+    pass
+
+
+class Booking:
+    pass
+
+
+class Payment:
+    pass
+
+
+class PaymentMethod:
+    pass
+
+
+class Bank(PaymentMethod):
+    pass
+
+
+class Card(PaymentMethod):
+    pass
+
+
+class Credit(Card):
+    pass
+
+
+class Debit(Card):
+    pass
+
+
+class Accomadation:
+    pass
+
+
+class Caledar:
     pass
