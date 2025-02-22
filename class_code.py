f2e17f0cfd463b7f7a05a3e451ef3923e436dc7d
Tawan67 <tawankhuntha@gmail.com>
2025-02-22 16:48:17 +0700

    update1

--- class_code.py
+++ class_code.py
@@ -6,20 +6,27 @@ class ControlSystem:
 class User:
     count_id = 0
 
-    def __init__(self, id, name, phone_num, age, payment_med):
+    def __init__(self, id, name, phone_num, age):
         self.__user_id = id
         self.__user_name = name
         self.__user_phone_num = phone_num
         self.__user_age = age
-        self.__user_payment_methhod = payment_med
+        self.count_id += 1
     pass
 
 
 class Member(User):
+    def __init__(self, id, name, phone_num, age, payment_med=None):
+        super().__init__(id, name, phone_num, age)
+        self.__pay_med = payment_med
     pass
 
 
 class Host(User):
+    def __init__(self, id, name, phone_num, age, payment_med):
+        super().__init__(id, name, phone_num, age)
+        self.__pay_med = payment_med
+
     pass
 
 
